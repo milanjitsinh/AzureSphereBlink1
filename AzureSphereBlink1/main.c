aeb1683f5a446823fe4123b61b023d121265c7ba
Milan Sindha <sindha.milan@gmail.com>
2019-10-25 13:14:37 +0530

    Add project files.

---
+++ AzureSphereBlink1/main.c
@@ -0,0 +1,30 @@
+﻿#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include <applibs/log.h>
+#include <applibs/gpio.h>
+
+int main(void)
+{
+	Log_Debug(
+		"\nVisit https://github.com/Azure/azure-sphere-samples for extensible samples to use as a "
+		"starting point for full applications.\n");
+
+	int fd = GPIO_OpenAsOutput(9, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (fd < 0) {
+		Log_Debug(
+			"Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+			strerror(errno), errno);
+		return -1;
+	}
+
+	const struct timespec sleepTime = { 1, 0 };
+	while (true) {
+		GPIO_SetValue(fd, GPIO_Value_Low);
+		nanosleep(&sleepTime, NULL);
+		GPIO_SetValue(fd, GPIO_Value_High);
+		nanosleep(&sleepTime, NULL);
+	}
+}
